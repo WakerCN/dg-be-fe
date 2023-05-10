dde764256444fa72affd2361f78a5d71e49ccce6
Waker <1209562577@qq.com>
2023-05-10 17:25:22 +0800

    chore 增加vuex

--- src/stores-vuex/index.ts
+++ src/stores-vuex/index.ts
@@ -1,12 +1,8 @@
 import { createStore } from 'vuex'
 
 const vuexStore = createStore({
-  state: { count: 0 },
-  mutations: {
-    add(state, payload) {
-      state.count++
-    }
-  }
+  state: {
+    count: 0
+  },
+  mutations: {}
 })
-
-export { vuexStore }
