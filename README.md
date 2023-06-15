f276c88ffbc560800adc099067185485ca2eb593
Waker <1209562577@qq.com>
2023-06-15 11:25:30 +0800

    docs 增加对vuex类型支持的文档

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-vuex由于没有维护，高版本node下需要手动修改node_modules中type的导出
+此项目为斗拱B端前端项目
+
+项目安装中后会有类型报错，是由于vuex由于没有维护，高版本node下需要手动修改node_modules中type的导出
 
 https://github.com/vuejs/vuex/issues/2213
+
+上面的方法有更好的解决方法
+
+https://github.com/vuejs/vuex/issues/2213#issuecomment-1592267216
--- src/types/vuex.d.ts
+++ src/types/vuex.d.ts
@@ -1,3 +1,8 @@
+/*
+ * @Author       : 魏威 <1209562577@qq.com>
+ * @Date         : 2023-05-11 10:55 周4
+ * @Description  : 
+ */
 import { Store } from 'vuex'
 import { GlobalStore } from '@/stores-vuex'
 
@@ -6,4 +11,11 @@ declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $store: Store<GlobalStore>
   }
+}
+
+declare module "vuex" {
+  export * from "vuex/types/index.d.ts";
+  export * from "vuex/types/helpers.d.ts";
+  export * from "vuex/types/logger.d.ts";
+  export * from "vuex/types/vue.d.ts";
 }
